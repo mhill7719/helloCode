4d46ce22d7186527f2b32a22b63705ccea0c7cce
Maegen Hill <maegen_hill@taylor.edu>
2025-05-21 00:06:20 -0400

    admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     end
   end
 
-def destroy
+  def destroy
     session[:user_id] = nil
     redirect_to store_index_url, notice: "Logged out"
   end
